7ce6fd3ff427bec13d2d14fc7cd5cfbee5fe11ef
Suganya <suganyaarul15@gmail.com>
2024-02-06 20:40:23 +0000

    11 sort-articles

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,20 +1,25 @@
 import Article from "./Article"
 import { getArticles } from "../utils/api"
 import {useSearchParams} from "react-router-dom"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 export default function ArticlesList({articles,setArticles}){
     const [searchParams]=useSearchParams()
+    const [isLoading, setIsLoading] =useState(true)
     const topic=searchParams.get('topic')
+    const sort_by=searchParams.get('sort_by')
+    const order=searchParams.get('order')
     useEffect(()=>{
-        if(topic!==undefined ){
-        getArticles(topic).then((body)=>{
+        getArticles(topic,sort_by,order).then((body)=>{
             setArticles(body)
+            setIsLoading(false)
         }).catch((error)=>{
             console.log(error.message);
         })
-    }
-},[])
+    
+},[topic, sort_by,order])
     return(
+        <>
+        {isLoading?<p>Loading Articles. Please wait</p>:
         <ol>
         { articles.length!==0?(
             articles.articles.map((article)=>{
@@ -22,6 +27,8 @@ export default function ArticlesList({articles,setArticles}){
             })
         ):<p>No Articles to show now.</p>}
         </ol>
+        }
+        </>
     )
 
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react"
 import UserContext from "../contexts/UserContext"
 import { Link } from "react-router-dom"
-export default function Header({topics,setQuery}){
+export default function Header({topics,setQuery,handleSort}){
     const loggedInUser = useContext(UserContext)
     function handleEvent(e){
         if(e.target.name==='article')
@@ -22,6 +22,18 @@ export default function Header({topics,setQuery}){
                     return <Link key={topic.slug} name={topic.slug} to={{pathname:'/articles',search:`?topic=${topic.slug}`}} onClick={handleEvent} className="head-link">{topic.slug}</Link>
                 })):null
                 }
+            </div>
+            <div>
+                <p>Sort: </p>
+                <select onChange={(e)=>{     
+                    handleSort(e.target.value)}}>
+                    <option value='created_at-desc'>Newest Date</option>
+                    <option value='created_at-asc'>Oldest Date</option>
+                    <option value='comment_count-desc'>Highest Comment_count</option>
+                    <option value='comment_count-asc'>Lowest Comment_count</option>
+                    <option value='votes-desc'>Highest Votes</option>
+                    <option value='votes-asc'>Lowest Votes</option>
+                </select>
             </div>
         </header>
     )
